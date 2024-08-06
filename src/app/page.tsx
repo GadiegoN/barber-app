ca407a7422aa6dfb30957f65a9ec2b9b7c6154ad
GadiegoN <ngadiego@gmail.com>
2024-08-06 20:01:19 -0300

    feat: add barbechop component and prisma client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+import { BarberShopItem } from "@/components/barber-shop-item"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { db } from "@/lib/prisma"
 import { Search } from "lucide-react"
 import Image from "next/image"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
-export default function Home() {
+export default async function Home() {
+  const barberShop = await db.barbershop.findMany()
+
   return (
     <div className="mx-auto flex w-11/12 max-w-7xl flex-col space-y-6">
       <div>
@@ -31,6 +42,10 @@ export default function Home() {
         />
       </div>
 
+      <h2 className="select-none text-xs font-bold uppercase text-gray-400">
+        Agendamentos
+      </h2>
+
       <Card>
         <CardContent className="flex h-32 justify-between p-0">
           <div className="flex flex-col justify-center gap-2 px-4">
@@ -52,6 +67,25 @@ export default function Home() {
           </div>
         </CardContent>
       </Card>
+
+      <h2 className="select-none text-xs font-bold uppercase text-gray-400">
+        Recomendados
+      </h2>
+
+      <Carousel className="mx-auto w-10/12 max-w-7xl md:w-11/12">
+        <CarouselContent className="gap-2">
+          {barberShop.map((barberShop) => (
+            <CarouselItem
+              key={barberShop.id}
+              className="basis-1/2 md:basis-1/3"
+            >
+              <BarberShopItem barbershop={barberShop} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   )
 }
---
+++ src/components/barber-shop-item.tsx
@@ -0,0 +1,43 @@
+import { Barbershop } from "@prisma/client"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { Badge } from "./ui/badge"
+import { Star } from "lucide-react"
+
+interface BarberShopItemProps {
+  barbershop: Barbershop
+}
+
+export async function BarberShopItem({ barbershop }: BarberShopItemProps) {
+  return (
+    <Card className="min-w-48 select-none rounded-2xl">
+      <CardContent className="space-y-3 p-0">
+        <div className="relative h-36 w-full">
+          <Image
+            alt={barbershop.name}
+            src={barbershop.imageUrl}
+            className="rounded-t-2xl object-cover"
+            fill
+          />
+          <Badge
+            variant="secondary"
+            className="absolute left-2 top-2 z-10 space-x-1"
+          >
+            <Star size={12} className="fill-primary text-primary" />
+            <p className="text-xs font-semibold">5.0</p>
+          </Badge>
+        </div>
+
+        <div className="mx-auto w-11/12 space-y-2">
+          <h3 className="line-clamp-1 font-semibold">{barbershop.name}</h3>
+          <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
+          <Button variant="secondary" className="w-full">
+            Reservar
+          </Button>
+          <div />
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
