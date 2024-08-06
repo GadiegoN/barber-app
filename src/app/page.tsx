44fcaf51fbd77c318bf52c3af312dd17f8d975ab
GadiegoN <ngadiego@gmail.com>
2024-08-06 16:40:48 -0300

    feat: add header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+import Image from "next/image"
+
 export default function Home() {
   return (
-    <main className="flex h-screen w-full justify-center items-center">
-      <p>ola mundo!</p>
-    </main>
-  );
+    <div className="mx-auto flex w-11/12 max-w-7xl flex-col space-y-6">
+      <div>
+        <h2 className="text-xl font-bold">Olá, Gadiego</h2>
+        <p>Terça-feira, 06 de agosto.</p>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Input placeholder="Faça sua busca..." />
+        <Button>
+          <Search />
+        </Button>
+      </div>
+
+      <div className="relative h-36 w-full md:h-64 lg:h-96">
+        <Image
+          alt="Agende nos melhores barbeiros da região"
+          src="/banner-01.png"
+          fill
+          quality={100}
+          className="rounded-xl object-cover"
+        />
+      </div>
+    </div>
+  )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { Menu } from "lucide-react"
+
+export function Header() {
+  return (
+    <Card>
+      <CardContent className="mx-auto flex w-11/12 max-w-7xl items-center justify-between py-5">
+        <Image
+          src="/logo.png"
+          height={18}
+          width={120}
+          alt="Logo da barbearia"
+        />
+
+        <Button variant="outline" size="icon">
+          <Menu />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
